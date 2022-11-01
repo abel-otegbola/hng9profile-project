69130473afef7c55ea2f2d2055db8e54a6c4c5ab
Abel <59369762+abel-otegbola@users.noreply.github.com>
2022-11-01 09:37:08 +0100

    Fix: made changes to Id of links

--- src/components/links/links.js
+++ src/components/links/links.js
@@ -4,17 +4,17 @@ const Links = () => {
     return (
         <div className="links">
 
-            <a href="https://twitter.com/Abel_Otegbola" id="twitter_link" title="twitter">Twitter Link</a>
+            <a href="https://twitter.com/Abel_Otegbola" id="twitter" title="twitter_link">Twitter Link</a>
 
             <a href="https://training.zuri.team/" id="btn_zuri" title="btn_zuri">Zuri Team</a>
 
-            <a href="https://books.zuri.team" id="books" title="zuri books">Zuri Books</a>
+            <a href="https://books.zuri.team" id="books" title="books">Zuri Books</a>
             
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=<abelotegbola>" id="book__python" title="python books">Python Books</a>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=abelotegbola" id="book__python" title="book__python">Python Books</a>
             
             <a href="https://background.zuri.team" id="pitch" title="pitch">Background Check for Coders</a>
             
-            <a href="https://books.zuri.team/design-rules" id="book__design" title="design books">Design Books</a>
+            <a href="https://books.zuri.team/design-rules" id="book__design" title="book__design">Design Books</a>
 
         </div>
     )
